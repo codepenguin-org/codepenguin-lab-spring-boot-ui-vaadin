6f36dcf5f92ec27aae0ce8c53f4a68d0e913b8dd
Jorge Garcia <jorgealfonsogarcia@gmail.com>
2022-09-06 21:24:56 -0500

    :tada: (feat) Initial commit.

---
+++ src/main/java/org/codepenguin/labs/CodepenguinLabSpringBootUiVaadinApplication.java
@@ -0,0 +1,13 @@
+package org.codepenguin.labs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodepenguinLabSpringBootUiVaadinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodepenguinLabSpringBootUiVaadinApplication.class, args);
+	}
+
+}
